c3f9d316d9469c481ba413b065625d200150a5f0
tim-hilt <timhilt@live.de>
2019-11-29 22:18:47 +0100

    Fixed alacritty.yml after update

--- .config/fish/functions/center_text.fish
+++
@@ -1,6 +0,0 @@
-# Defined in - @ line 1
-function center_text
-	set termwidth (tput cols)
-    set padding (printf '%0.1s' ={1..500})
-    printf '%*.*s %s %*.*s\n' 0 "(((termwidth-2-{#1})/2))" "$padding" "$1" 0 "(((termwidth-1-{#1})/2))" "$padding"
-end
